2802417b09691283d0c701a9384aa829991a8434
user <user@user-PC>
2017-05-25 08:51:01 +0900

    score ing

--- group_prj/src/kr/co/sist/recipe/dao/ScoreDAO.java
+++ group_prj/src/kr/co/sist/recipe/dao/ScoreDAO.java
@@ -1,5 +1,17 @@
 package kr.co.sist.recipe.dao;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
 import kr.co.sist.recipe.vo.ScoreVO;
 
 public class ScoreDAO {
@@ -13,11 +25,100 @@ public class ScoreDAO {
 		return score_dao;
 	}//getInstance
 	
-	// 상품보기 팝업 - 평점주기
-	public boolean insertScore(ScoreVO scoreVo){
-		return false;
+	private Connection getConnection() throws SQLException {
+	      Connection con = null;
+	      //지용이인 내가 수정 하였다!!!!!!! merge 하였음
+	      Properties prop = new Properties();
+	      try {
+	         File file = new File("C:/dev/workspace/group_prj2/src/kr/co/sist/recipe/dao/recipe_db.properties");
+	         
+	         if (file.exists()) {
+	            prop.load(new FileInputStream(file));
+	            String driver = prop.getProperty("driver");
+	            String url = prop.getProperty("url");
+	            String id = prop.getProperty("dboid");
+	            String pass = prop.getProperty("dbopwd");
+	            System.out.println(driver);
+
+	            try {
+	               Class.forName(driver);
+	               con = DriverManager.getConnection(url, id, pass);
+	            } catch (ClassNotFoundException e) {
+	               e.printStackTrace();
+	            } // end catch
+
+	         } else {
+	            JOptionPane.showMessageDialog(null, "설정파일의 경로를 확인해주세요");
+	         } // end else
+
+	      } catch (FileNotFoundException fe) {
+	         fe.printStackTrace();
+	      } catch (IOException ie) {
+	         ie.printStackTrace();
+	      } // end catch
+
+	      return con;
+	   }// getConnection
+	
+	
+	///////////////////// 상품보기 팝업 - 평점주기/////////////////////
+	public boolean insertScore(String id, String menu_name, int value) throws SQLException{
+		boolean result=false;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try{
+			//1. 드라이버로딩
+			//2. 커넥션 얻기
+			con = getConnection();
+			//3. 쿼리문 생성객체 얻기
+			String insertPoint = "insert into score(id,menu_name,value) values(?,?,?)";
+			pstmt = con.prepareStatement(insertPoint);
+			//4. 쿼리수행 후 결과얻기
+				// 바인드변수에 값 설정
+			
+			pstmt.setString(1, id);
+			pstmt.setString(2, menu_name);
+			pstmt.setInt(3, value);
+			
+			pstmt.executeUpdate();
+			result=true;
+			
+		}finally {
+			//5.
+			if(pstmt!=null){ pstmt.close(); }
+			if(con!=null){ con.close(); }
+		}//end finally
+		
+		
+		if(result){
+			System.out.println("추가되었음");
+		}else{
+			System.out.println("추가 안되었음");
+			
+		}
+		return result;
+		
 	}//insertScore
 	
+////////////////////단위테스트///////////////////////////////////////////	
+	public static void main(String[] args){
+		String id="duck";
+		String menu_name="오지 치즈 후라이";
+		int value=3;
+		
+		ScoreDAO sd = new ScoreDAO();
+		
+		try {
+			sd.insertScore(id, menu_name, value);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+////////////////////단위테스트///////////////////////////////////////////	
 	// 상품보기 팝업 - 평점 갱신
 	public boolean updateScore(ScoreVO scoreVo){
 		return false;
